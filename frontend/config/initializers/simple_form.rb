2fc4d53dcb572de1bd80bdd005d1ded8799c744b
Israel Barba <israel.barba@facturasi.com>
2013-12-04 21:35:53 -0600

    views for the log in restaurants

--- frontend/config/initializers/simple_form.rb
+++ frontend/config/initializers/simple_form.rb
@@ -1,17 +1,19 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
+  
+
   # Wrappers are used by the form builder to generate a
   # complete input. You can remove any component from the
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+  config.wrappers :inline_checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
     # You can make any of these extensions optional by
     # renaming `b.use` to `b.optional`.
+    
 
     # Determines whether to use HTML5 (:email, :url, ...)
     # and required attributes
